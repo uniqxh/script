efeac50850c94484640a18ee2fdcf90a2adf9af0
uniqxh <525799145@qq.com>
2017-06-12 14:07:59 +0800

    update

--- sdccppt.py
+++ sdccppt.py
@@ -4,9 +4,9 @@
 import urllib
 import urllib2
 import json
-import re
 import os
 
+#直接访问下载链接，跳过登录限制，这里存在SQL注入
 list_url = "http://download.csdn.net/index.php/meeting/speechlist/?sid=85%20or%20sid=86"
 down_url = "http://meet.download.csdn.net/speech"
 
